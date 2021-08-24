016cc2b831b6b6ac3431d7fd5d5c5b4a68aee9a4
RybalovValentyn <0661629069@ukr.net>
2021-08-24 11:47:39 +0300

    page number

--- src/index.js
+++ src/index.js
@@ -75,6 +75,7 @@ function onFetchError() {
 };
 
 function clearImagesList() {
+  pageNum = 1;
   imagesRef.innerHTML = '';
 }
 
@@ -110,4 +111,12 @@ async function onImageModalServis() {
   }
 }
 
+
+window.onload = function () {
+  // document.body.classList.add('loaded_hiding');
+  window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    // document.body.classList.remove('loaded_hiding');
+  }, 500);
+}
 
